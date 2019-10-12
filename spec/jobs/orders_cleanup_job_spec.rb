a00f8beb333c221f2b1317c50482aa47e99a57ac
Denis <denis@efojs.com>
2019-10-12 16:03:01 +0300

    Fix job spec to check request

--- spec/jobs/orders_cleanup_job_spec.rb
+++ spec/jobs/orders_cleanup_job_spec.rb
@@ -3,16 +3,25 @@ require 'rails_helper'
 RSpec.describe OrdersCleanupJob, type: :job do
   describe "#perform_later" do
     context 'gets properly enqueued' do
+      # to perform "post" request in example
+      include RSpec::Rails::RequestExampleGroup
+
       it 'gets enqueued' do
         ActiveJob::Base.queue_adapter = :test
         expect { OrdersCleanupJob.perform_later("order") }.to have_enqueued_job
       end
-      it 'enqueued for correct time' do
+      it 'enqueued via /book request for correct time' do
         ActiveJob::Base.queue_adapter = :test
-        time = Time.now
-        expect {
-          OrdersCleanupJob.set(wait_until: time+900).perform_later("order")
-        }.to have_enqueued_job.with("order").on_queue("default").at(time + 900)
+
+        event = create(:event)
+        post "/api/v1/events/#{event.id}/book/1"
+        json = JSON.parse(response.body)
+        # to catch if request didn't succeed
+        expect(response).to have_http_status(200)
+        expect(json).not_to be_empty
+        @order = Order.find(json["id"])
+
+        expect(OrdersCleanupJob).to have_been_enqueued.with(@order).on_queue("default").at(@order.expires_at)
       end
     end
     context 'performs job' do
